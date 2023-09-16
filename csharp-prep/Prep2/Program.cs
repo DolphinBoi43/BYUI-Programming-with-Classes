05208e8d23ffc38b2106141cdc97038d8d210e54
DolphinBoi43 <screwboi25@gmail.com>
2023-09-16 08:31:53 -0600

    Finished the Core Tasks and cleaned it up

--- csharp-prep/Prep2/Program.cs
+++ csharp-prep/Prep2/Program.cs
@@ -4,6 +4,44 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello Prep2 World!");
+        Console.Write("Enter grade percentage: ");
+        string gradepercent = Console.ReadLine();
+        string letter = ("F");
+        int passing = 0;
+        int grade = int.Parse(gradepercent);
+
+        if (grade >= 90)
+        {
+            letter = ("A");
+        }
+        else if (grade >= 80 && grade < 90)
+        {
+            letter = ("B");
+        }
+        else if (grade >= 70 && grade < 80)
+        {
+            letter = ("C");
+        }
+        else if (grade >= 60 && grade < 70)
+        {
+            letter = ("D");
+        }
+        else if (grade < 60)
+        {
+            letter = ("F");
+        }
+        if (grade < 70)
+        {
+            passing = 1;
+        }
+        Console.Write($"Your grade is {letter}, ");
+        if (passing != 1)
+        {
+            Console.Write("you are passing");
+        }
+        else
+        {
+            Console.Write("you are not passing");
+        }
     }
 }
