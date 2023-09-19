0e405a7603d69ea3d3d57ed5764f6bedafe2604f
DolphinBoi43 <screwboi25@gmail.com>
2023-09-19 13:14:39 -0600

    Prep 3 Attempt #1

--- csharp-prep/Prep3/Program.cs
+++ csharp-prep/Prep3/Program.cs
@@ -4,6 +4,20 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello Prep3 World!");
+        Random magic_num_gen = new Random();
+        int guess_try = 0;
+        int guess_num = 0;
+        int magic_num = magic_num_gen.Next(1,100);
+
+        do
+        {
+            Console.WriteLine($"What number am I thinking of?{magic_num}:");
+            string guess = Console.ReadLine();
+            guess_try = guess_try++;
+            guess_num = int.Parse(guess);
+        } while (guess_num != magic_num);
+
+        Console.WriteLine($"You got it!\nNumber of Attempts: {guess_try}");
+
     }
 }
