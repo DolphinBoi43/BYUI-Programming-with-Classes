5ba1bdc3f1036a8bdd137006096cedfae65785eb
DolphinBoi43 <screwboi25@gmail.com>
2023-10-12 20:32:19 -0600

    Finished all requirements

--- prepare/Learning03/Program.cs
+++ prepare/Learning03/Program.cs
@@ -1,9 +1,23 @@
 using System;
-
 class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello Learning03 World!");
+        Fraction one = new Fraction();
+        Fraction two = new Fraction(5);
+        Fraction three = new Fraction(3, 4);
+        Fraction four = new Fraction (1, 3);
+        // Fraction 1
+        Console.WriteLine(one.GetFractionString());
+        Console.WriteLine(one.GetDecimalValue());
+        // Fraction 5
+        Console.WriteLine(two.GetFractionString());
+        Console.WriteLine(two.GetDecimalValue());
+        // Fraction 3/4
+        Console.WriteLine(three.GetFractionString());
+        Console.WriteLine(three.GetDecimalValue());
+        // Fraction 1/3
+        Console.WriteLine(four.GetFractionString());
+        Console.WriteLine(four.GetDecimalValue());
     }
 }
