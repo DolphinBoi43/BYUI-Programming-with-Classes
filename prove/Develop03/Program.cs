e33f1c56b3017972fa106b6b51c1ad8fb88f1bcb
DolphinBoi43 <screwboi25@gmail.com>
2023-11-07 23:04:26 -0700

    Progress

--- prove/Develop03/Program.cs
+++ prove/Develop03/Program.cs
@@ -4,7 +4,13 @@ class Program
 {
     static void Main()
     {
-        List<string> Scriptures = "2 Nephi_29_(8-10).txt", "Ether_12_(4).txt"];
+        Random rnd = new Random();
+        string[] Files= new string[2];
+            Files[0] = "2 Nephi_29_8-10.txt";
+            Files[1] = "Ether_12_4.txt";
+        string memorizeScript = Files[rnd.Next(0,1)];
+        
+
 
     }
 }
--- prove/Develop03/Scripture.cs
+++ prove/Develop03/Scripture.cs
@@ -1,34 +1,4 @@
-using System;
-using System.IO;
 class Scripture
 {
-    // public void HideRandomWord()
-    // {
-    //     string[] words = text.Split(' ');
-    //     int randomIndex;
-    //     do
-    //     {
-    //         randomIndex = new Random().Next(words.Length);
-    //     } while (hiddenWords.Contains(words[randomIndex]));
-    //     hiddenWords.Add(words[randomIndex]);
-    // }
 
-    // public bool AreAllWordsHidden()
-    // {
-    //     string[] words = text.Split(' ');
-    //     return hiddenWords.Count == words.Length;
-    // }
-
-    // public string GetHiddenScripture()
-    // {
-    //     string[] words = text.Split(' ');
-    //     for (int i = 0; i < words.Length; i++)
-    //     {
-    //         if (hiddenWords.Contains(words[i]))
-    //         {
-    //             words[i] = new string('_', words[i].Length);
-    //         }
-    //     }
-    //     return $"{reference.Text}\n{string.Join(" ", words)}";
-    // }
 }
