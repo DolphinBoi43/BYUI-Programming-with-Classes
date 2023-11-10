786e4cfcb55b0c05283fef61ae051e0ba707f6f7
DolphinBoi43 <screwboi25@gmail.com>
2023-11-09 21:42:42 -0700

    Starting off

--- prove/Develop04/Program.cs
+++ prove/Develop04/Program.cs
@@ -4,6 +4,8 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello Develop04 World!");
+        Console.WriteLine("Welcome to the Mindfulness Companion App");
+        Console.WriteLine("What would you like to do?");
+        Console.WriteLine("1) Breathe\n2) List\n3) Reflect");
     }
 }
