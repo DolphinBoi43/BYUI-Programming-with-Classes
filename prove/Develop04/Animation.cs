eda0edb0d1806990bc92360e89adf867d57077ac
DolphinBoi43 <screwboi25@gmail.com>
2023-11-09 21:57:46 -0700

    Back to Basics

--- prove/Develop04/Animation.cs
+++ prove/Develop04/Animation.cs
@@ -1,24 +1,13 @@
 public class Animation
 {
     int counter;
-    public ConsoleSpiner()
+    public Animation()
     {
         counter = 0;
     }
-    public void Turn()
+    
+    public void CatBlink()
     {
-        counter++;        
-        switch (counter % 4)
-        {
-            case 0: Console.Write("/"); break;
-            case 1: Console.Write("-"); break;
-            case 2: Console.Write("\\"); break;
-            case 3: Console.Write("|"); break;
-        }
-        Console.SetCursorPosition(Console.CursorLeft - 1, Console.CursorTop);
-    }
-    public void Clear()
-    {
-        Console.Clear();
+        
     }
 }
--- prove/Develop04/Program.cs
+++ prove/Develop04/Program.cs
@@ -2,15 +2,13 @@ using System;
 
 class Program
 {
+    void Clear()
+    {
+        Console.Clear();
+    }
     static void Main(string[] args)
     {
-        Animation.Clear();
-        ConsoleSpiner spin = new ConsoleSpiner();
-        Console.Write("Working....");
-        while (true) 
-        {
-            spin.Turn();
-        }
+        Clear();
         Console.WriteLine("Welcome to the Mindfulness Companion App");
         Console.WriteLine("What would you like to do?");
         Console.WriteLine("1) Breathe\n2) List\n3) Reflect");
