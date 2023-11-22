6a5b657ba154ddf413503b74c7520bfc9b569f94
DolphinBoi43 <screwboi25@gmail.com>
2023-11-22 10:32:27 -0700

    Just gettting started

--- final/Foundation1/Program.cs
+++ final/Foundation1/Program.cs
@@ -4,6 +4,57 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello Foundation1 World!");
+        // Video goes
+        // title / author / length / number of comments
+        // Comment goes
+        // User / user comment
+        Video Youtube_1 = new Video();
+        Comment Com_1 = new Comment();
+        Comment Com_1_1 = new Comment();
+        Comment Com_1_2 = new Comment();
+        Com_1._user = ""; 
+        Com_1_1._user = ""; 
+        Com_1_2._user = "";
+        Com_1._ucomment = ""; 
+        Com_1_1._ucomment = ""; 
+        Com_1_2._ucomment = "";
+        Youtube_1._title = "";
+        Youtube_1._author = "";
+        Youtube_1._length = 0.0;
+        Youtube_1._comment_num = 1;
+
+        Video Youtube_2 = new Video();
+        Comment Com_2 = new Comment();
+        Comment Com_2_1 = new Comment();
+        Comment Com_2_2 = new Comment();
+        Com_2._user = "";
+        Com_2_1._user = "";
+        Com_2_2._user = "";
+        Com_2._ucomment = "";
+        Com_2_1._ucomment = "";
+        Com_2_2._ucomment = "";
+        Youtube_2._title = "";
+        Youtube_2._author = "";
+        Youtube_2._length = 0.0;
+        Youtube_2._comment_num = 1;
+
+        Video Youtube_3 = new Video();
+        Comment Com_3 = new Comment();
+        Comment Com_3_1 = new Comment();
+        Comment Com_3_2 = new Comment();
+        Com_3._user = "";
+        Com_3_1._user = "";
+        Com_3_2._user = "";
+        Com_3._ucomment = "";
+        Com_3_1._ucomment = "";
+        Com_3_2._ucomment = "";
+        Youtube_3._title = "";
+        Youtube_3._author = "";
+        Youtube_3._length = 0.0;
+        Youtube_3._comment_num = 1;
+
+        Youtube_1.DisplayVideo();
+        Youtube_2.DisplayVideo();
+        Youtube_3.DisplayVideo        
     }
 }
--- final/Foundation1/Video.cs
+++ final/Foundation1/Video.cs
@@ -1,7 +1,7 @@
 public class Video
 {
-    public string _title;
-    private string _author; private float _length; private int _comment_num;
+    public List<Comment> _comments = new List<Comment>();
+    public string _title, _author; public float _length; public int _comment_num;
     public Video (title, author, length, comment_num)
     {
         _title = title;
@@ -9,19 +9,18 @@ public class Video
         _length = length;
         _comment_num = comment_num;
     }
-    // title / author / length / number of comments
-    public (string, string, float, int) NewVideo()
-    {
-
-        return (_title, _author, _length, _comment_num); 
-    }
-
-    public void DisplayVideo(List<>)
+// title / author / length / number of comments
+    public void DisplayVideo()
     {
         Console.WriteLine($"{_title} by {_author}\n{_length:.2f} mins || {_comment_num} Comments");
-        for (int i = 0; i < _comment_num; i++)
+        DisplayComments();
+    }
+    public void DisplayComments()
+    {
+        Console.WriteLine("Comments:");
+        foreach (Comment comment in _comments)
         {
-            if 
+            comment.DisplayComment();
         }
     }
 }
